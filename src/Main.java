9b639191767df23533fe5cc7d90776e5bce9eff5
ALex ATMAS <atmasbooking@gmail.com>
2022-04-22 00:06:38 +0300

    Выполнено!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hw8.dz1();
+        Hw8.dz2();
+        Hw8.dz3();
+        Hw8.dz4();
+        Hw8.dz4_1();
+        Hw8.dz5();
+
+
     }
 }
